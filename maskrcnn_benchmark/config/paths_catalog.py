7df18482cd3cdcdd81cd2588c07e1b8f2274be8d
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-03-23 10:52:06 -0400

    Update paths_catalog.py

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -14,11 +14,11 @@ class DatasetCatalog(object):
         "coco_2017_val": ("coco/val2017", "coco/annotations/instances_val2017.json"),
         "coco_vertebres_train": (
           "coco/images_SJ",
-          "coco/annotations/train_segmentation_SJ_K1.json",
+          "coco/annotations/train_segmentation_SJ_K1v2.json",
         ),
         "coco_vertebres_test": (
           "coco/images_SJ",
-          "coco/annotations/test_segmentation_SJ_K1.json",
+          "coco/annotations/test_segmentation_SJ_K1v2.json",
         ),
     }
     # DATASETS = {
